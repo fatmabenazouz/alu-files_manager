d2d198dc6d74e758c96622c63b163b3591b2c366
fatmabenazouz <f.benazouz@alustudent.com>
2024-12-02 22:50:56 +0200

    edits

--- models/index.js
+++ models/index.js
@@ -1,24 +1,20 @@
 'use strict';
 
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const { Sequelize } = require('sequelize');
 
-
-const app = express();
-
-
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: false })); 
-
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Server is running!' });
+const sequelize = new Sequelize('file_manager_app', 'app_user', 'secure_password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: false, 
 });
 
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to MySQL has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
-app.use((req, res, next) => {
-  const error = new Error('Not Found');
-  error.status = 404;
-  next(error);
-});
+module.exports = { sequelize };
